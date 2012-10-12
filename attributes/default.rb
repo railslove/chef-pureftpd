7715875b6e14996d8bdb9c9fcadaca6d6da4a216
Lars Brillert <lars@railslove.com>
2012-10-12 17:12:43 +0200

    inital commit

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default[:pureftpd][:auth_addon] = "mysql" # specifiy addons eg mysql, postgresql or ldap
+default['pureftpd']['dir'] = "/etc/pure-ftpd"
+default[:pureftpd][:private_key_path] = "/etc/ssl/private"
+default[:pureftpd][:tls] = 1
+default[:pureftpd][:chroot_everyone] = "yes"
+default[:pureftpd][:create_home_dir] = "yes"
+default[:pureftpd][:passive_port_range] = "29799 29899"
---
+++ recipes/default.rb
@@ -0,0 +1,55 @@
+#
+# Cookbook Name:: pureftpd
+# Recipe:: default
+#
+# Copyright 2012, Railslove GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+pureftpd_service = ["pure-ftpd", node[:pureftpd][:auth_addon]].join("-")
+
+package "pure-ftpd"
+
+package "pure-ftpd-#{node[:pureftpd][:auth_addon]}" if node[:pureftpd][:auth_addon]
+
+service pureftpd_service do
+  supports :status => true, :restart => true
+  action [ :enable, :start ]
+end
+
+cookbook_file "#{node['pureftpd']['private_key_path']}/pure-ftpd.pem" do
+  owner 'root'
+  group 'root'
+  mode 0600
+end
+
+file "#{node['pureftpd']['dir']}/conf/TLS" do
+  content node['pureftpd']['tls'].to_s
+  notifies :restart, resources(:service => pureftpd_service), :delayed
+end
+
+file "#{node['pureftpd']['dir']}/conf/ChrootEveryone" do
+  content node['pureftpd']['chroot_everyone']
+  notifies :restart, resources(:service => pureftpd_service), :delayed
+end
+
+file "#{node['pureftpd']['dir']}/conf/CreateHomeDir" do
+  content node['pureftpd']['create_home_dir']
+  notifies :restart, resources(:service => pureftpd_service), :delayed
+end
+
+file "#{node['pureftpd']['dir']}/conf/PassivePortRange" do
+  content node['pureftpd']['passive_port_range']
+  notifies :restart, resources(:service => pureftpd_service), :delayed
+end
